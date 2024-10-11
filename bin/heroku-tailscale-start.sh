eeb4e398ad092ce150eea0b4d3bf4a899708fe81
Jonathan Siegel <248302+usiegj00@users.noreply.github.com>
2024-10-11 19:44:16 +0900

    Trying nohup instead of setsid.

--- bin/heroku-tailscale-start.sh
+++ bin/heroku-tailscale-start.sh
@@ -33,7 +33,7 @@ else
   log "Using Tailscale hostname=$tailscale_hostname"
 
   log "Disown the tailscaled process so that it does not occupy the same session id and hang the shell (in the new exec.d execution environment)."
-  setsid tailscaled -verbose ${TAILSCALED_VERBOSE:-0} --tun=userspace-networking --socks5-server=localhost:1055 --socket=/tmp/tailscaled.sock &
+  nohup tailscaled -verbose ${TAILSCALED_VERBOSE:-0} --tun=userspace-networking --socks5-server=localhost:1055 --socket=/tmp/tailscaled.sock &
   until tailscale --socket=/tmp/tailscaled.sock \
     up \
     --authkey=${TAILSCALE_AUTH_KEY} \
@@ -49,6 +49,4 @@ else
 
   export ALL_PROXY=socks5://localhost:1055/
   log "Tailscale started"
-  exec 3>&-
-  log "Closed env stream."
 fi
